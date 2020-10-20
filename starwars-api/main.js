8c1cc8358c6eece5fdfb92b5a3742309d7edb706
Jackson Rosser <10839138@uvu.edu>
2020-10-20 12:37:54 -0600

    chewsday

--- starwars-api/main.js
+++ starwars-api/main.js
@@ -1,3 +1,17 @@
 import { films } from './data/films.js'
+import {people} from './data/people.js'
 
-console.log(films[6])
+
+
+const main = document.querySelector("main")
+
+people.forEach(person => {
+
+    console.log(person.name)
+    let newTitle = main.appendChild(document.createElement("h1"))
+    newTitle.textContent = person.name
+
+    let personImg = document.createElement('img')
+    personImg.src = person.url
+    main.appendChild(person.img)
+}); 
