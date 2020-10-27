08094d6c1951ef78ad75b1872c5a8459bc47dfaf
Jackson Rosser <10839138@uvu.edu>
2020-10-27 12:49:49 -0600

    Checking Format, Issue with loading api pages

--- star-wars-characters/main.js
+++ star-wars-characters/main.js
@@ -2,46 +2,47 @@ import { people } from '../data/people.js'
 
 const mainContent = document.querySelector('#main')
 
-const mainHeader = document.body.insertBefore(mainHeader, mainContent)
-mainHeader.className = "mainHeader"
-document.body.appendChild(mainHeader)
+const mainHeader = document.createElement('header')
+mainHeader.className = 'mainHeader'
+document.body.insertBefore(mainHeader, mainContent)
 
 const maleButton = document.createElement('button')
 maleButton.textContent = 'Male Characters'
 mainHeader.appendChild(maleButton)
 
 const femaleButton = document.createElement('button')
-maleButton.textContent = 'Female Characters'
+femaleButton.textContent = 'Female Characters'
 mainHeader.appendChild(femaleButton)
 
 const otherButton = document.createElement('button')
-maleButton.textContent = 'OtherCharacters'
+otherButton.textContent = 'Other Characters'
 mainHeader.appendChild(otherButton)
 
-const maleCharacters = people.filter(person=> person.gender === 'male')
+const maleCharacters = people.filter(person => person.gender === 'male')
 console.log(maleCharacters)
 
-const femaleCharacters = people.filter(person=> person.gender === 'female')
-const otherCharacters 
-
 maleButton.addEventListener('click', (event) => {
-    maleCharacters.forEach(person=>{
+    maleCharacters.forEach(person => {
         const charFigure = document.createElement('figure')
         const charImg = document.createElement('img')
-        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${person.url}.jpg`
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/10.jpg`
         const charCaption = document.createElement('figcaption')
         charCaption.textContent = person.name
-
+    
         charFigure.appendChild(charImg)
         charFigure.appendChild(charCaption)
+    
         mainContent.appendChild(charFigure)
     })
 })
+
 let theURL = "https://swapi.co/api/people/1/"
-function getLastNumber (url){
+
+function getLastNumber(url) {
     console.log(url)
-    str(url)
-    return[-2]
 }
+
 getLastNumber(theURL)
+
 //https://starwars-visualguide.com/assets/img/characters/1.jpg
+//"url": "https://swapi.co/api/people/1/"
