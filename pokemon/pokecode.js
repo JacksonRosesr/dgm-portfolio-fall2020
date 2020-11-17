a3ba9d4702e651257577d03f110af35d8f16384f
Jackson Rosser <10839138@uvu.edu>
2020-11-17 12:48:54 -0700

    Created Images, error in displaying pokeGrid

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,23 +1,64 @@
-const pokeGrid = document.querySelector('.pokeGrid')
-
-
-async function loadData(){
-    const response= await fetch('http://pokeapi.co/api/v2/pokemone/')
+// Reusable async function to fetch data from the provided url
+async function getAPIData(url){
+    //try-catch error block
+    try { 
+    const response= await fetch(url)
     const data = await response.json()
-    populatePokePage(data)
-}
-function populatePokePage(data){
-    const allPokemon = data.results
-    for ( const pokemon of allPokemon){
-
-    
-    let pokeCard = document.createElement('div')
-    pokeCard.className = "pokecard"
-    let pokeName = document.createElement('h3')
-    pokeName.textContent = pokemon.name
-
-    pokeCard.appendChild(pokeName)
-    pokeGrid.appendChild(pokeCard)
+    return(data)
+    }
+    catch (error){
+        console.error(error)
     }
 }
-loadData()
+
+//now use async getAPIData function
+
+function loadPage(data){
+    getAPIData('https://pokeapi.co/api/v2/pokemon').then (async(data)=>{
+        for (const pokemon of data.results){
+            await getAPIData(pokemon.url).then((pokeData)=>
+            {populatePokecard(pokeData)
+            })
+            
+        }
+    })
+}
+
+const pokeGrid = document.querySelector('.pokeGrid')
+
+function populatePokecard(singlePokemon){
+    console.log(singlePokemon)
+    let pokeScene= document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = document.createElement('div')
+    pokeCard.classname = 'card'
+    pokeCard.addEventListener('click', function(){
+        pokeCard.classList.toggle('is-flipped')
+    })
+    let pokeFront = document.createElement('div')
+    let pokeBack = document.createElement('div')
+
+    let bakcLabel = document.createElement('p')
+
+    bakcLabel.textContent = `${singlePokemon.moves.length} moves`
+    pokeBack.appendChild(backLabel)
+
+    let frontLabel = document.createElement('p')
+    frontLabel.textContent = singlePokemon.name
+    let frontImg = document.createElement('img')
+    frontImg.src  = `../images/pokeImages/00${getImageFileName(pokemon)}.png`
+
+    pokeFront.appendChild(frontLabel)
+    pokeCard.appendChild(pokeFront)
+    pokeCard.appendChild(pokeBack)
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
+}
+function getImageFileName(pokemon){
+    if (pokemon.id < 10){
+        return `00${pokemon.id}`
+    }
+    else if (pokemon.id>9 && pokemon.id < 100){
+        return `0${pokemon.id}`
+    }
+}
